bd397ce9de6027de2f839a57ad5fc8688e2113ab
Kota Suzuki <shadowsdev.d12@gmail.com>
2013-02-03 15:16:52 +0900

    PCバッテリー情報取得処理追加

--- BatteryStatus/AppController.m
+++ BatteryStatus/AppController.m
@@ -12,23 +12,202 @@
 
 - (void)awakeFromNib
 {
+    [self performSelectorInBackground:@selector(createBatteryStatusText) withObject:nil];
+}
+
+-(void)createBatteryStatusText
+{
     BatteryStatus *status = [[BatteryStatus alloc] init];
+    
+    // バッテリーの接続状況取得
+    BOOL extCon = [status getNoteBatteryExternalConnect];
+    NSString *externalConnected = [[NSString alloc] init];
+    if (extCon) {
+        externalConnected =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"EXTERNAL_CONNECTED", @""),
+             NSLocalizedString(@"YES", @"")];
+    }
+    else {
+        externalConnected =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"EXTERNAL_CONNECTED", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
+    // バッテリーの充電状況取得
+    BOOL batFully = [status getNoteBatteryIsFully];
+    NSString *batteryIsFully = [[NSString alloc] init];
+    if (batFully) {
+        batteryIsFully =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_IS_FULLY", @""),
+             NSLocalizedString(@"YES", @"")];
+    }
+    else {
+        batteryIsFully =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_IS_FULLY", @""),
+             NSLocalizedString(@"NO", @"")];
+    }
+    
+    // バッテリー残り時間取得
+    NSString *batteryTimeLeft = [status getNoteBatteryTimeLeft];
+    if (![batteryTimeLeft isEqualToString:@""]) {
+        batteryTimeLeft =
+            [NSString stringWithFormat:@"%@ %@min\n",
+             NSLocalizedString(@"BATTERY_TIMELEFT", @""),
+             batteryTimeLeft];
+    }
+    else {
+        batteryTimeLeft =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_TIMELEFT", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
+    // バッテリー充電率取得
+    NSString *batteryPersent = [status getNoteBatteryPersent];
+    if (![batteryPersent isEqualToString:@""]) {
+        batteryPersent =
+            [NSString stringWithFormat:@"%@ %@%%\n",
+             NSLocalizedString(@"BATTERY_PERSENT", @""),
+             batteryPersent];
+    }
+    else {
+        batteryPersent =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_PERSENT", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
+    // バッテリー健康度取得
+    NSString *batteryHelth = [status getNoteBatteryHelth];
+    if (![batteryHelth isEqualToString:@""]) {
+        batteryHelth =
+            [NSString stringWithFormat:@"%@ %@%%\n",
+             NSLocalizedString(@"BATTERY_HELTH", @""),
+             batteryHelth];
+    }
+    else {
+        batteryHelth =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_HELTH", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
+    // バッテリー容量取得
+    NSString *batteryCurrentCapacity = [status getNoteBatteryCurrentCapacity];
+    if (![batteryCurrentCapacity isEqualToString:@""]) {
+        batteryCurrentCapacity =
+            [NSString stringWithFormat:@"%@ %@mAh\n",
+             NSLocalizedString(@"BATTERY_CURRENT_CAPACITY", @""),
+             batteryCurrentCapacity];
+    }
+    else {
+        batteryCurrentCapacity =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_CURRENT_CAPACITY", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
+    // バッテリー最大容量取得
+    NSString *batteryMaxCapacity = [status getNoteBatteryMaxCapacity];
+    if (![batteryMaxCapacity isEqualToString:@""]) {
+        batteryMaxCapacity =
+            [NSString stringWithFormat:@"%@ %@mAh\n",
+             NSLocalizedString(@"BATTERY_MAX_CAPACITY", @""),
+             batteryMaxCapacity];
+    }
+    else {
+        batteryMaxCapacity =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_MAX_CAPACITY", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
+    // バッテリーデザイン容量取得
+    NSString *batteryDegignCapacity = [status getNoteBatteryDegignCapacity];
+    if (![batteryDegignCapacity isEqualToString:@""]) {
+        batteryDegignCapacity =
+            [NSString stringWithFormat:@"%@ %@mAh\n",
+             NSLocalizedString(@"BATTERY_DEGIGN_CAPACITY", @""),
+             batteryDegignCapacity];
+    }
+    else {
+        batteryDegignCapacity =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BATTERY_DEGIGN_CAPACITY", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
     
     // BTキーボードのバッテリー残量取得
     NSString *keybordBattery = [status getBTKeyboardBatteryPersent];
-    keybordBattery = [NSString stringWithFormat:@"%@ %@\n", NSLocalizedString(@"BTKEYBORD_BATTERY", @""), keybordBattery];
+    if (![keybordBattery isEqualToString:@""]) {
+        keybordBattery =
+            [NSString stringWithFormat:@"%@ %@%%\n",
+             NSLocalizedString(@"BTKEYBORD_BATTERY", @""),
+             keybordBattery];
+    }
+    else {
+        keybordBattery =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BTKEYBORD_BATTERY", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
     
     // BTマウスのバッテリー残量取得
     NSString *mouseBattery = [status getBTMouseBatteryPersent];
-    mouseBattery = [NSString stringWithFormat:@"%@ %@\n", NSLocalizedString(@"BTMOUSE_BATTERY", @""), mouseBattery];
+    if (![mouseBattery isEqualToString:@""]) {
+        mouseBattery =
+            [NSString stringWithFormat:@"%@ %@%%\n",
+             NSLocalizedString(@"BTMOUSE_BATTERY", @""),
+             mouseBattery];
+    }
+    else {
+        mouseBattery =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BTMOUSE_BATTERY", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
+    
     
     // BTトラックパッドのバッテリー残量取得
     NSString *trackpadBattery = [status getBTTrackpadBatteryPersent];
-    trackpadBattery = [NSString stringWithFormat:@"%@ %@\n", NSLocalizedString(@"BTTRACKPAD_BATTERY", @""), trackpadBattery];
+    if (![trackpadBattery isEqualToString:@""]) {
+        trackpadBattery =
+            [NSString stringWithFormat:@"%@ %@%%\n",
+             NSLocalizedString(@"BTTRACKPAD_BATTERY", @""),
+             trackpadBattery];
+    }
+    else {
+        trackpadBattery =
+            [NSString stringWithFormat:@"%@ %@\n",
+             NSLocalizedString(@"BTTRACKPAD_BATTERY", @""),
+             NSLocalizedString(@"NOT_CONNECT", @"")];
+    }
     
-    displayText = [NSString stringWithFormat:@"%@%@%@",keybordBattery, mouseBattery, trackpadBattery];
+    // テキストを一気に連結
+    displayText =
+        [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@",
+         externalConnected,
+         batteryIsFully,
+         batteryTimeLeft,
+         batteryPersent,
+         batteryHelth,
+         batteryCurrentCapacity,
+         batteryMaxCapacity,
+         batteryDegignCapacity,
+         keybordBattery,
+         mouseBattery,
+         trackpadBattery];
     
-    // テキストビューに貼り付け
+    [self performSelectorOnMainThread:@selector(updateDisplayTextView) withObject:nil waitUntilDone:NO];
+}
+
+-(void)createBatteryStatusTextDidfinish
+{
     [self updateDisplayTextView];
 }
 
